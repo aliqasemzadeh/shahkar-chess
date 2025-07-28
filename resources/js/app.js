c4a01f89380f6606b64eb3c76037647bbb24024a
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2025-07-28 21:50:26 +0330

    Import chess module into app.js for chessboard integration.

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,8 +3,7 @@ import './../../vendor/power-components/livewire-powergrid/dist/tailwind.css';
 import 'flatpickr/dist/flatpickr.min.css';
 import flatpickr from "flatpickr";
 
-
-
+import './chess';
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
